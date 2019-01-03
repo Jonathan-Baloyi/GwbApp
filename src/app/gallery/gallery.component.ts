f3afe031ba5c5d313428a224d29c0d890979b1ca
Jonathan Baloyi <jonathan.baloyi@dariel.co.za>
2019-01-03 12:54:47 +0200

    Quick Fix

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -8,25 +8,25 @@ import { IImage } from 'ng-simple-slideshow';
 })
 export class GalleryComponent implements OnInit {
 
-  imageSources: IImage[] = [
-    {url: '../assets/images/bg5.jpeg', backgroundSize: '700px'},
-    {url: '../assets/images/bg4.jpeg', backgroundSize: '700px'},
-    {url: '../assets/images/bg6.jpeg', backgroundSize: '700px'},
-    {url: '../assets/images/img2.jpg', backgroundSize: '700px'},
-    {url: '../assets/images/img4.jpg', backgroundSize: '700px'},
-    {url: '../assets/images/img5.jpg', backgroundSize: '700px'}
-  ];
+  // imageSources: IImage[] = [
+  //   {url: '../assets/images/bg5.jpeg', backgroundSize: '700px'},
+  //   {url: '../assets/images/bg4.jpeg', backgroundSize: '700px'},
+  //   {url: '../assets/images/bg6.jpeg', backgroundSize: '700px'},
+  //   {url: '../assets/images/img2.jpg', backgroundSize: '700px'},
+  //   {url: '../assets/images/img4.jpg', backgroundSize: '700px'},
+  //   {url: '../assets/images/img5.jpg', backgroundSize: '700px'}
+  // ];
 
   /** When Deploying to github or any other hosting environment, Image url's should be as follows */
 
-  // imageSources: IImage[] = [
-  //   {url: '../GwbApp/assets/images/bg5.jpeg', backgroundSize: '700px'},
-  //   {url: '../GwbApp/assets/images/bg4.jpeg', backgroundSize: '700px'},
-  //   {url: '../GwbApp/assets/images/bg6.jpeg', backgroundSize: '700px'},
-  //   {url: '../GwbApp/assets/images/img2.jpg', backgroundSize: '700px'},
-  //   {url: '../GwbApp/assets/images/img4.jpg', backgroundSize: '700px'},
-  //   {url: '../GwbApp/assets/images/img5.jpg', backgroundSize: '700px'}
-  // ];
+  imageSources: IImage[] = [
+    {url: '../GwbApp/assets/images/bg5.jpeg', backgroundSize: '700px'},
+    {url: '../GwbApp/assets/images/bg4.jpeg', backgroundSize: '700px'},
+    {url: '../GwbApp/assets/images/bg6.jpeg', backgroundSize: '700px'},
+    {url: '../GwbApp/assets/images/img2.jpg', backgroundSize: '700px'},
+    {url: '../GwbApp/assets/images/img4.jpg', backgroundSize: '700px'},
+    {url: '../GwbApp/assets/images/img5.jpg', backgroundSize: '700px'}
+  ];
 
   constructor() { }
 
